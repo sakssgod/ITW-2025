86f8748a117573acefa6c431c8522b416cbac0b2
Viktori09 <you@example.com>
2025-04-04 12:00:18 +0100

    js

---
+++ script/demo_player.js
@@ -0,0 +1,12 @@
+function Player(nick, score, time, password){
+    this.nick = nick;
+    this.score = score;
+    this.time = time;
+    this.password = password;
+}
+
+let PLAYER = [
+    new Player("Rubbito22", "111", "225"),
+    new Player("Vikiki", "20", "113"),
+    new Player("Oujie", "55", "186")
+];
---
+++ script/tabuleiro.js
@@ -0,0 +1,11 @@
+function Difficulty(rows, colums, number_mines, time_limit){
+    this.rows= rows;
+    this.colums = colums;
+    this.number_mines = number_mines;
+    this.time_limit = time_limit;
+
+}
+
+let DIFFICULTY = [
+    new D
+]
