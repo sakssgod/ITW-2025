2d4fd1f211b86b95b7dc8901d4d0f89accb931fd
Ruben Pereira <133230419+Rubbito@users.noreply.github.com>
2025-05-16 11:25:14 +0100

    Tabuleeeeiro

--- script/tabuleiro.js
+++ script/tabuleiro.js
@@ -1,11 +1,27 @@
-function Difficulty(rows, colums, number_mines, time_limit){
-    this.rows= rows;
-    this.colums = colums;
+function Difficulty(rows, columns, number_mines, time_limit) {
+    this.rows = rows;
+    this.columns = columns;
     this.number_mines = number_mines;
     this.time_limit = time_limit;
-
 }
 
 let DIFFICULTY = [
-    new D
-]
+    new Difficulty(15, 15, 30, 300)
+];
+
+
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    const gameBoard = document.getElementById("gameBoard");
+    const currentDifficulty = DIFFICULTY[0];
+    const rows = currentDifficulty.rows;
+    const cols = currentDifficulty.columns;
+
+    for (let i = 0; i < rows * cols; i++) {
+        const div = document.createElement("div");
+        div.classList.add("cell");
+        gameBoard.appendChild(div);
+    }
+});
